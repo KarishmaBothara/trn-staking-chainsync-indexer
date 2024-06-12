fdc13030d5956a3ad11ce0e634301ca914c49043
Kun Wang <kun.w@hotmail.com>
2024-06-12 16:06:34 +1200

    update config

--- src/config.ts
+++ src/config.ts
@@ -6,13 +6,13 @@ export const config = {
   datasource: {
     archive:
       process.env.ARCHIVE_URL ||
-      "https://otto-gateway.ottoblockchain.network/graphql",
+      "http://squid-gateway-service.vortex.svc.cluster.local:8000/graphql",
     chain:
       process.env.CHAIN_URL ||
-      "wss://api.plugdefi.app/otto-testnet-tahi/ws?apikey=e03dd150-7a9c-4f43-9d90-3f9aa4501427",
+      "wss://porcini.au.rootnet.app/archive/ws",
   },
   startBlock: process.env.START_BLOCK
     ? Number(process.env.START_BLOCK)
-    : 920000,
+    : 0,
   chainPrefix: Number(process.env.CHAIN_PREFIX) || 42,
 };
